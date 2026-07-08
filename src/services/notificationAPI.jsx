36f3ff7c33b4a0c988fb057362dd532aed8c2eda
happyn9 <nephtalitwite12@gmail.com>
2026-07-08 22:43:10 +0200

    hfront  ajout notif,del prof, center management student   V1.0.1

--- src/services/notificationAPI.jsx
+++ src/services/notificationAPI.jsx
@@ -1,16 +1,16 @@
 import api from "./api";
 
 export const getNotifications = () =>
-  api.get("/api/notifications").then((res) => res.data);
+  api.get("/api/notifications");
 
 export const markAsRead = (id) =>
-  api.patch(`/api/notifications/${id}/read`).then((res) => res.data);
+  api.patch(`/api/notifications/${id}/read`);
 
 export const markAllAsRead = () =>
-  api.patch("/api/notifications/read-all").then((res) => res.data);
+  api.patch("/api/notifications/read-all");
 
 export const subscribePush = (subscription) =>
-  api.post("/api/notifications/subscribe", subscription).then((res) => res.data);
+  api.post("/api/notifications/subscribe", subscription);
 
 export const unsubscribePush = (endpoint) =>
-  api.delete("/api/notifications/unsubscribe", { params: { endpoint } }).then((res) => res.data);
+  api.delete("/api/notifications/unsubscribe", { params: { endpoint } });
